e93be55ef0238f55162843134b255e137067070d
fadouaabdoul <fadwaabdoul@gmail.com>
2022-08-17 16:12:14 +0100

    tryna add a carousel

--- todolist/src/App.vue
+++ todolist/src/App.vue
@@ -1,17 +1,20 @@
 <template>
     <headermain/>
+    <slider/>
     <todolist/>
 </template>
 
 <script>
 import todolist from './components/todolist.vue'
 import headermain from './components/header.vue'
+import slider from './components/slider'
 
 export default {
   name: 'App',
   components: {
     todolist,
-      headermain
+      headermain,
+      slider
   }
 }
 </script>
---
+++ todolist/src/components/slider.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>slider</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "sliderCarousel"
+    }
+</script>
+
+<style scoped>
+
+</style>
